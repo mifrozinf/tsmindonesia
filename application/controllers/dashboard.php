8c9881aa88f6b036dd596ff07649c9731aae8ce6
nurtannio <mnurtannio25@gmail.com>
2018-02-06 16:02:56 +0700

    menampilkan product dari dashboard controller

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -1,25 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class dashboard extends CI_Controller {
+class Dashboard extends CI_Controller {
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('M_product');
+	}
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
-	{
+	{			
 		$this->load->view('frontend/header');
 		$this->load->view('frontend/index');
 		$this->load->view('frontend/footer');
@@ -33,13 +22,15 @@ class dashboard extends CI_Controller {
 	public function contact()
 	{
 		$this->load->view('frontend/header');
-		$this->load->view('frontend/index');
+		$this->load->view('frontend/contact');
 		$this->load->view('frontend/footer');
 	}
 	public function products()
 	{
+		$data['dataProduct'] = $this->M_product->select_all();
+
 		$this->load->view('frontend/header');
-		$this->load->view('frontend/index');
+		$this->load->view('frontend/products', $data);
 		$this->load->view('frontend/footer');
 	}
 }
--- application/views/frontend/products.php
+++ application/views/frontend/products.php
@@ -1,15 +1,16 @@
+<?php foreach ($dataProduct as $row) : ?>
 <section class="page-section">
   <div class="container">
     <div class="product-item">
       <div class="product-item-title d-flex">
         <div class="bg-faded p-5 d-flex ml-auto rounded">
           <h2 class="section-heading mb-0">
-            <span class="section-heading-upper">Blended to Perfection</span>
-            <span class="section-heading-lower">Coffees &amp; Teas</span>
+<!--             <span class="section-heading-upper"><?php echo $row->name; ?></span> -->
+            <span class="section-heading-lower"><?php echo $row->name; ?></span>
           </h2>
         </div>
       </div>
-      <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url(); ?>assets/frontend/img/products-01.jpg" alt="">
+      <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url(); ?>assets/frontend/img/<?php echo $row->name; ?>" alt="">
       <div class="product-item-description d-flex mr-auto">
         <div class="bg-faded p-5 rounded">
           <p class="mb-0">We take pride in our work, and it shows. Every time you order a beverage from us, we guarantee that it will be an experience worth having. Whether it's our world famous Venezuelan Cappuccino, a refreshing iced herbal tea, or something as simple as a cup of speciality sourced black coffee, you will be coming back for more.</p>
@@ -18,45 +19,4 @@
     </div>
   </div>
 </section>
-
-<section class="page-section">
-  <div class="container">
-    <div class="product-item">
-      <div class="product-item-title d-flex">
-        <div class="bg-faded p-5 d-flex mr-auto rounded">
-          <h2 class="section-heading mb-0">
-            <span class="section-heading-upper">Delicious Treats, Good Eats</span>
-            <span class="section-heading-lower">Bakery &amp; Kitchen</span>
-          </h2>
-        </div>
-      </div>
-      <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url(); ?>assets/frontend/img/products-02.jpg" alt="">
-      <div class="product-item-description d-flex ml-auto">
-        <div class="bg-faded p-5 rounded">
-          <p class="mb-0">Our seasonal menu features delicious snacks, baked goods, and even full meals perfect for breakfast or lunchtime. We source our ingredients from local, oragnic farms whenever possible, alongside premium vendors for specialty goods.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-<section class="page-section">
-  <div class="container">
-    <div class="product-item">
-      <div class="product-item-title d-flex">
-        <div class="bg-faded p-5 d-flex ml-auto rounded">
-          <h2 class="section-heading mb-0">
-            <span class="section-heading-upper">From Around the World</span>
-            <span class="section-heading-lower">Bulk Speciality Blends</span>
-          </h2>
-        </div>
-      </div>
-      <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url(); ?>assets/frontend/img/products-03.jpg" alt="">
-      <div class="product-item-description d-flex mr-auto">
-        <div class="bg-faded p-5 rounded">
-          <p class="mb-0">Travelling the world for the very best quality coffee is something take pride in. When you visit us, you'll always find new blends from around the world, mainly from regions in Central and South America. We sell our blends in smaller to large bulk quantities. Please visit us in person for more details.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+<?php endforeach; ?>
