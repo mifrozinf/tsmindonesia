c7aa337976246d36bbb52f485e3674ee89796c79
nurtannio <mnurtannio25@gmail.com>
2018-02-07 15:20:47 +0700

    edit kontak dan about
    selainnya mindahin nav nya ke setiap kodingan biar active

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -31,29 +31,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       <span class="site-heading-lower">YOUR DAILY CINCAU</span>
   </h1>
 
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav mx-auto">
-            <li class="nav-item px-lg-4">
-              <a class="nav-link text-uppercase text-expanded" href="<?php echo base_url(); ?>index.php">Home
-              </a>
-          </li>
-          <li class="nav-item px-lg-4">
-              <a class="nav-link text-uppercase text-expanded" href="<?php echo base_url().'index.php/dashboard/about' ?>">About</a>
-          </li>
-          <li class="nav-item px-lg-4">
-              <a class="nav-link text-uppercase text-expanded" href="<?php echo base_url().'index.php/dashboard/products' ?>">Products</a>
-          </li>
-          <li class="nav-item px-lg-4">
-              <a class="nav-link text-uppercase text-expanded" href="<?php echo base_url().'index.php/dashboard/contact' ?>">Contact</a>
-          </li>
-      </ul>
-  </div>
-</div>
-</nav>
+  
