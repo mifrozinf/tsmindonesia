25f244226c40ad9a47ba6c62dc5fdb88bb6ced48
nurtannio <mnurtannio25@gmail.com>
2018-02-08 17:38:45 +0700

    ini edit halaman depan

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -17,8 +17,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link href="<?php echo base_url(); ?>assets/frontend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
-    <link href="<?php echo base_url(); ?>https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="<?php echo base_url(); ?>assets/frontend/css/business-casual.min.css" rel="stylesheet">
@@ -27,8 +27,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <body>
     <h1 class="site-heading text-center text-white d-none d-lg-block">
-      <span class="site-heading-upper text-primary mb-3">TSM CINCAU OFFICIAL</span>
-      <span class="site-heading-lower">YOUR DAILY CINCAU</span>
-  </h1>
+        <!--   <img src="<?php echo base_url(); ?>assets/img/logo.jpg" class="gambar"></img><br> -->
+        <span class="site-heading-lower">TSM INDONESIA</span>
+    </h1>
+    <style type="text/css">
+    .gambar{
+        height: 125px;
+        width: 125px;
+    }
+</style>
 
-  
+
--- application/views/frontend/index.php
+++ application/views/frontend/index.php
@@ -20,23 +20,46 @@
     </div>
   </div>
 </nav>
-<section class="page-section about-heading">
+
+<section class="page-section clearfix">
   <div class="container">
-    <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="<?php echo base_url(); ?>assets/img/logo.jpg; ?>" alt="">
-    <div class="about-heading-content">
-      <div class="row">
-        <div class="col-xl-9 col-lg-10 mx-auto">
-          <div class="bg-faded rounded p-5">
-<!--             <h2 class="section-heading mb-4">
-              <span class="section-heading-lower">Home Profile</span>
-            </h2>
- -->
-            <p><strong>PT. Tunas Sari Mulia</strong> Merupakan Perusahaan yang bergerak dalam bidang minuman dengan produk utama adalah minuman cincau siap minum. Kantor pusat PT. Tunas Sari Mulia bermarkas di Purwokerto.  Perusahaan ini berawal dengan sistem manajemen keluarga dan berusaha memanfaatkan tanaman cincau yang berada di Indonesia ini. Perusahaan ini bergerak dibawah lisensi Tan Soon Mui Food Industries Singapura.
-            </p>
-            <p class="mb-0">Salah satu produk utama yang dihasilkan yaitu minuman cincau SenSoh Cinco-To yang siap untuk langsung diminum. Bahan yang digunakan merupakan bahan baku cincau alami, gula, air dan perasa pandan. Untuk menjaga kualitas produk kami menerapkan sistem kebijakan halal pada perusahaan dimulai dari pembuatan produk hingga produk sampai ke konsumen dan telah mendapatkan sertifikasi halal dari Majelis Ulama Indonesia dan terdaftar pada Badan Pengawas Obat Dan Makanan.</p>
-          </div>
+    <div class="intro">
+      <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="<?php echo base_url();?>assets/frontend/img/intro.jpg" alt="">
+      <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+        <h2 class="section-heading mb-4">
+          <span class="section-heading-upper">Cincau Hitam</span>
+          <span class="section-heading-lower">TSM Indonesia</span>
+        </h2>
+        <p class="mb-3">Minuman cincau dengan gaya berbeda, rasakan kenikmatan yang unik disetiap sedotan cincau hitam yang manis.
+          dengan ini kami berkomitmen untuk menjaga kehalalan produk, rasa produk sampai ke tangan konsumen.
+        </p>
+        <div class="intro-button mx-auto">
+          <a class="btn btn-primary btn-xl" href="#">Visit Us Today!</a>
         </div>
       </div>
     </div>
   </div>
 </section>
+
+<section class="page-section cta">
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-9 mx-auto">
+        <div class="cta-inner text-center rounded">
+          <h2 class="section-heading mb-4">
+            <span class="section-heading-upper">Home Profile</span>
+          </h2>
+          <p class="text-justify"><strong>PT. Tunas Sari Mulia</strong> Merupakan Perusahaan yang bergerak dalam bidang minuman dengan produk utama adalah minuman cincau siap minum. Kantor pusat PT. Tunas Sari Mulia bermarkas di Purwokerto. Perusahaan ini berawal dengan sistem manajemen keluarga dan berusaha memanfaatkan tanaman cincau yang berada di Indonesia ini. Perusahaan ini bergerak dibawah lisensi Tan Soon Mui Food Industries Singapura.</p>
+          <p class="text-justify">Salah satu produk utama yang dihasilkan yaitu minuman cincau TSM SenSoh Cincau Hitam yang siap untuk langsung diminum. Bahan yang digunakan merupakan bahan baku cincau alami, gula, air dan perasa pandan. Untuk menjaga kualitas produk kami menerapkan sistem kebijakan halal pada perusahaan dimulai dari pembuatan produk hingga produk sampai ke konsumen dan telah mendapatkan sertifikasi halal dari Majelis Ulama Indonesia dan terdaftar pada Badan Pengawas Obat Dan Makanan.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<style type="text/css">
+.center-text {
+  text-align: justify;
+  text-justify: inter-word;
+}
+}
+</style>
