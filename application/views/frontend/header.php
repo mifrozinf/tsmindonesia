abea86a1b4a247b5564a70ca4ba7c3d00a08c93f
nurtannio <mnurtannio25@gmail.com>
2018-02-09 14:43:41 +0700

    edit css

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -14,14 +14,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <title>TSM CINCAU</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url(); ?>assets/frontend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/frontend/vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="<?php echo base_url(); ?>assets/frontend/css/business-casual.min.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/frontend/css/business-casual.css'); ?>" rel="stylesheet">
     <link rel="icon" type="image/png" href="<?php echo base_url(); ?>assets/img/logo.jpg">
 </head>
 
