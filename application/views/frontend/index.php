d18c093a997dc21674c8707c36b4e28fb309bfe9
nurtannio <mnurtannio25@gmail.com>
2018-02-12 09:09:59 +0700

    ganti foto

--- application/views/frontend/index.php
+++ application/views/frontend/index.php
@@ -24,7 +24,7 @@
 <section class="page-section clearfix">
   <div class="container">
     <div class="intro">
-      <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="<?php echo base_url();?>assets/frontend/img/intro.jpg" alt="">
+      <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="<?php echo base_url();?>assets/frontend/img/intro.jpeg" alt="">
       <div class="intro-text left-0 text-center bg-faded p-5 rounded">
         <h2 class="section-heading mb-4">
           <span class="section-heading-upper">Cincau Hitam</span>
